f1932926a3891c4aad43fd8c26fc09fe30ff6b06
Elmmoo <lyagami.light@gmail.com>
2025-04-26 18:26:12 +0500

    Up

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,17 @@
 package app.model;
 
-public class Timer {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Timer {
+    private static final Timer instance = new Timer();
     private Long nanoTime = System.nanoTime();
+
+    private Timer() {}
+
+    public static Timer getInstance() {
+        return instance;
+    }
 
     public Long getTime() {
         return nanoTime;
